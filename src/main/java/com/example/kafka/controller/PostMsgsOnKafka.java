807a1a9f1b79f9faa77789f20f4e4371b3b22499
Rishabh <rishabhchaurasiya007@gmail.com>
2025-05-21 23:50:34 +0530

    first commit

---
+++ src/main/java/com/example/kafka/controller/PostMsgsOnKafka.java
@@ -0,0 +1,27 @@
+package com.example.kafka.controller;
+
+import com.example.kafka.setvice.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class PostMsgsOnKafka {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @PostMapping("/send")
+
+    public String sendMessageToKafka(
+            @RequestParam int start,
+            @RequestParam int end,
+            @RequestHeader("id") String id) {
+        for (int i = start; i <= end; i++) {
+            String message = "Message for iteration: " + i;
+            kafkaProducer.sendMessage("Uniper_Topic", message, id);
+        }
+        return "Messages sent to Kafka for range: " + start + " to " + end;
+    }
+}
---
+++ src/main/java/com/example/kafka/setvice/KafkaProducer.java
@@ -0,0 +1,19 @@
+package com.example.kafka.setvice;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    public void sendMessage(String topic, String message, String id) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+        record.headers().add("id", id.getBytes());
+        kafkaTemplate.send(record);
+    }
+}
